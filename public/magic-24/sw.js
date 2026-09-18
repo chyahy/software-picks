a0d57c3444a1c7185e8a7b98812c4d6b5e85ddb1
chyahy <136792675+chyahy@users.noreply.github.com>
2026-09-18 11:13:52 +0800

    Add automatic cache refresh for prime factor game

--- public/magic-24/sw.js
+++ public/magic-24/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'dangdang-magic-24-2026.09.18.1';
+const CACHE_VERSION = 'dangdang-magic-24-2026.09.18.2';
 
 self.addEventListener('install', () => {
     self.skipWaiting();
---
+++ public/prime-factor/sw.js
@@ -0,0 +1,21 @@
+const CACHE_VERSION = 'prime-factor-2026.09.18.2';
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key.startsWith('prime-factor-') && key !== CACHE_VERSION).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode !== 'navigate') return;
+  event.respondWith(
+    fetch(new Request(event.request, { cache: 'reload' }))
+      .catch(() => fetch(event.request))
+  );
+});
